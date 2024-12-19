e523453ce09e32bab9d0c7d6c5f776fb8b38e232
depay-cyber <lyonkasirye@gmail.com>
2024-12-19 18:33:03 +0300

    alphabetifier

---
+++ alpabet.py
@@ -0,0 +1,10 @@
+#Lets check if you know the alphabet
+print('Hi')
+character  =str(input('Type in any keyboard character to check if its an alphabet letter'))
+print('character typed: ', character)
+alphabet =('abcdefghijklmnopqrstuvwxyz') 
+if character=  (\alphabet[1:26 ]):
+    print('good ')
+else:
+    print('try again you failed')    
+
