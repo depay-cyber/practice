ce5f1554baa2e1d18b07124c73aa1eab73797e53
depay-cyber <lyonkasirye@gmail.com>
2024-12-19 16:40:52 +0300

    conditional statments

--- num.py
+++ num.py
@@ -1,5 +1,13 @@
-num1 = 20
-num2 = 47
-num3 = 57
-sum = num1 + num2 + num3
-print(sum)
+# num1 = 20
+# num2 = 47
+# num3 = 57
+# sum = num1 + num2 + num3
+# print(sum)
+
+#check if the number is odd or even
+num = int(input('enter number '))
+print('number entered: ', num )
+if num % 2 == 0 :
+    print('number is even')
+else : 
+    print('number is odd')    
