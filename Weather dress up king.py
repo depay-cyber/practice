9e8330e7fcd56fd05727d866384931febf8016e5
depay-cyber <lyonkasirye@gmail.com>
2024-12-19 18:18:57 +0300

    Dress up to the weather

---
+++ Weather dress up king.py
@@ -0,0 +1,12 @@
+#Decide what to waer according to the weather
+temperature = float(input('whats the temperatutre at your place '))
+print('Temperature is: ', temperature)
+if temperature <=29:
+    print('wear heavy clothes since its cold outside my nigga')
+elif temperature <=41:
+    print('wear modrate clothes since its modrate temperature my nigga')
+elif temperature <=57:
+    print('wear light clothes since its hot outside my nigga')
+else :
+    print('wear very light and reflective clothes since its a desert outside')
+                
