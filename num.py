22112d2a9c7f22e21a08d86d5a961470e51204bf
depay-cyber <lyonkasirye@gmail.com>
2024-12-19 17:43:24 +0300

    BMI doctor

--- num.py
+++ num.py
@@ -5,9 +5,25 @@
 # print(sum)
 
 #check if the number is odd or even
-num = int(input('enter number '))
-print('number entered: ', num )
-if num % 2 == 0 :
-    print('number is even')
-else : 
-    print('number is odd')    
+# num = int(input('enter number '))
+# print('number entered: ', num )
+# if num % 2 == 0 :
+#     print('number is even')
+# else : 
+#     print('number is odd')    
+
+#Caluculate BMI
+height = float(input('enter your height'))
+weight = float(input('enter your weight'))
+BMI = weight / (height/100)**2
+print('your BMI is: ', BMI)
+if BMI<= 18.4:
+    print('you are underwight')
+elif BMI<= 24.9:
+    print('you are normal')
+elif BMI<=34.9:
+    print('your obese')
+else:
+    print('your extreme obese')
+        
+                
