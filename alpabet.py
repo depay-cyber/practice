1499618b88128b01beef753767a6e45cf81c5ed6
depay-cyber <lyonkasirye@gmail.com>
2024-12-20 16:13:01 +0300

    error clear

--- alpabet.py
+++ alpabet.py
@@ -3,7 +3,7 @@ print('Hi')
 character  =str(input('Type in any keyboard character to check if its an alphabet letter'))
 print('character typed: ', character)
 alphabet =('abcdefghijklmnopqrstuvwxyz') 
-if character=  (\alphabet[1:26 ]):
+if character==  alphabet[1:26 ]:
     print('good ')
 else:
     print('try again you failed')    
