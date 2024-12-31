0a7faf6193783fa24ccd6529448a0c2b84d9bbaf
depay-cyber <lyonkasirye@gmail.com>
2024-12-31 17:02:28 +0300

    divisibility

---
+++ Divisible.py
@@ -0,0 +1,6 @@
+num = int(input('Enter a number to check if its divisible by 2: '))
+if num % 2 ==0: 
+    print('number is divisible by 2')
+else: 
+    print('number is not divisible by 2')
+               
