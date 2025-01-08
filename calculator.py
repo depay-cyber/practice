f23896bc8387e391c3126b9ab3f27ca88de77928
depay-cyber <lyonkasirye@gmail.com>
2025-01-08 16:41:39 +0300

    calc 2

--- calculator.py
+++ calculator.py
@@ -1,4 +1,33 @@
-print('lets divide by 2')
-num=int(input('enter the number: '))
-ans= num/2
-print (ans)
+# print('lets divide by 2')
+# num=int(input('enter the number: '))
+# ans= num/2
+# print (ans)
+def add(num1, num2):
+    return(num1 + num2)
+def subtract(num1 , num2):
+    return(num1 - num2)
+def divide(num1, num2):
+    return(num1 / num2)
+def multiply(num1,num2):
+    return(num1/ num2)
+print ('please select the operation')
+print('a. add, n\ b. subtract ,n\c. divide, n\d.  multiply ')
+choice=input('Please emter choice a or b or c or d ')
+num1 =int(input('Please enter the firt number'))
+num2=int(input('please enter second number'))
+if choice=='a':
+    print(num1 , '+', num2, '=', add(num1, num2))
+elif choice=="b":
+    print(num1 , '-', num2, '=', subtract(num1, num2))
+elif choice == 'c':
+    print(num1 , '/', num2, '=', divide(num1, num2)) 
+elif choice== 'd':
+    print(num1 , '*', num2, '=', multiply(num1, num2)) 
+else:
+    print('invalid input')
+             
+    
+
+
+
+
