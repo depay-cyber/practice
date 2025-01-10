b4e995150e7eb3a6f6e0020be2dc6aaaacd9ee95
depay-cyber <lyonkasirye@gmail.com>
2025-01-10 16:39:38 +0300

    circumference

---
+++ circumference.py
@@ -0,0 +1,10 @@
+print('lets check out the circumference of a circle')
+diameter=float(input('Enter the diameter of the circle: '))
+pie=3.14
+def circumference(pie, diameter):
+    return(pie*diameter)
+print('Circumference =',circumference(pie, diameter))
+
+
+
+
