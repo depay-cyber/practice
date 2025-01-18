0116e75df21aa96d606fffdaae6196f0768655a1
depay-cyber <lyonkasirye@gmail.com>
2025-01-18 13:25:56 +0300

    ager

--- agey.py
+++ agey.py
@@ -1,8 +1,15 @@
 try:
-    year= (input('Enter your birth month and year seperated by commas '))
-    print(year)
+    year= int(input('Enter your birth  year  '))
+    print('Birth year is', + year)
 except(SyntaxError) :
     print('invalid input') 
 except(NameError):
-    print('wrong try again')      
+    print('wrong try again')
+except(ValueError):
+    print('wrong values boss, try again ') 
+age= 2025 - year
+if age < 0 :
+    print('Error , Error 67 occured') 
+else:
+    print('Your age is', + age)                
 
