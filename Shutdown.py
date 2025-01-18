d391dad2efc63a32ae360cb0a292e3b03948fe2a
depay-cyber <lyonkasirye@gmail.com>
2025-01-18 13:55:56 +0300

    shut

--- Shutdown.py
+++ Shutdown.py
@@ -1,1 +1,8 @@
-def Shutdown()
+def Shutdown():
+    return('shuting down')
+dtermine = (input('is chrome,system app , or any other app running (yes or no): '))
+if dtermine == 'no':
+    print('shutting down')
+else:
+    print('cant shut down unless apps closed')
+Shutdown()     
---
+++ maonths.py
@@ -0,0 +1,3 @@
+import calendar
+ def itermonthdays():
+     return
