49ce15f0c049142cbb9c12cdc42ba7723dc0094f
depay-cyber <lyonkasirye@gmail.com>
2025-01-20 16:45:02 +0300

    fk

--- game.py
+++ game.py
@@ -2,10 +2,14 @@ import pygame
 pygame.init()
 #screen
 screen=pygame.display.set_mode((400,500))
+screen.fill((67,46,9))
+
 done=False
 while not done:
     for event in pygame.event.get():
         if event.type== pygame.QUIT:
             pygame.quit()
-    pygame.display.flip()        
-    
+        pygame.draw.rect(screen,(24,99,147),pygame.Rect(30,30,60,60))    
+    pygame.display.flip()
+            
+
